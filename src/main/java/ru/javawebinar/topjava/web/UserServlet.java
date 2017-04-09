510b0fb9242d9aae280e8b2e89ffa9d784ec09f7
Stas Cheslavskyi <stas.hess@gmail.com>
2017-04-09 16:21:59 +0300

    add logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,16 +1,25 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+
 /**
  * Created by Admin on 08.04.2017.
  */
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(UserServlet.class);
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to users");
 
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("users.jsp");
     }
 }
